ead15e77e8c4dbae12f4ac6d82e87379f551376e
KAVISHKA4811 <155081773+KAVISHKA4811@users.noreply.github.com>
2025-11-01 11:17:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~UdZimQbR#VGBvudJ-GEvajm_dR_LA6Nidyks4WCvx8hn1Ggh3HTg'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~BF5hkRCZ#eyL1z3Oy53UyH9KtiX1LgN4-rsVk19WZm8Ouxzg_PjI'
 };
